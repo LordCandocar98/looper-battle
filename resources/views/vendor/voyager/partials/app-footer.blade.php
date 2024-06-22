ce3abb30e92add43468bec2d0b1356dd21f78898
root <root@vps.server.local>
2024-06-22 19:56:44 +0000

    Logica para el detalle de la partida con ganador para todos contra todos o por equipos

---
+++ resources/views/vendor/voyager/partials/app-footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="app-footer">
+    <div class="site-footer-right">
+        {!! __('voyager::theme.footer_copyright') !!} <a href="#" target="_blank">Looper VR</a>
+    </div>
+
+</footer>
